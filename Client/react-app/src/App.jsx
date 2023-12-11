d671120cf906068d2b5917992b2e6c617d34d8b0
Zsombor Pocs <142238433+Zsombinszky@users.noreply.github.com>
2023-12-11 14:54:36 +0100

    locations are shows on the homepage

--- Client/react-app/src/App.jsx
+++ Client/react-app/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import Location from "./components/Location";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/location");
+        const jsonData = await response.json();
+        setData(jsonData.results);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      <h1>Locations</h1>
+      <ul>
+        {data.map((location, index) => (
+          <Location key={index} name={location.name} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
